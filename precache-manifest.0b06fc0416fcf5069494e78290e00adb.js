5a7feb9506870e6ba6ff8f1b255bf18f75386c26
pandulaDW <pandula.official@gmail.com>
2020-02-02 09:47:56 +0530

    Updates

---
+++ precache-manifest.0b06fc0416fcf5069494e78290e00adb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b87d6bdeab8e561cf4ffbf0d6cc9f4d",
+    "url": "/react-hangman-game/index.html"
+  },
+  {
+    "revision": "84dd1930e3713621ea0b",
+    "url": "/react-hangman-game/static/css/main.12cc93a3.chunk.css"
+  },
+  {
+    "revision": "4be7b4cc491fdbe98cc4",
+    "url": "/react-hangman-game/static/js/2.34117433.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-hangman-game/static/js/2.34117433.chunk.js.LICENSE"
+  },
+  {
+    "revision": "84dd1930e3713621ea0b",
+    "url": "/react-hangman-game/static/js/main.80ec516a.chunk.js"
+  },
+  {
+    "revision": "9ebe219e4fdb5e3f1eb2",
+    "url": "/react-hangman-game/static/js/runtime-main.396876c1.js"
+  },
+  {
+    "revision": "81cffcb25260651d0d63e35439beeca0",
+    "url": "/react-hangman-game/static/media/background.81cffcb2.jpg"
+  },
+  {
+    "revision": "8c7aacdd739759f2c657ff3e9b8fd2de",
+    "url": "/react-hangman-game/static/media/hangman3.8c7aacdd.png"
+  }
+]);
