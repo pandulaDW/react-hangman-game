7564d175fe97d35078641e544c1c1f20063118c7
pandulaDW <pandula.official@gmail.com>
2020-02-02 09:43:17 +0530

    Added more words

--- src/words.js
+++ src/words.js
@@ -1,5 +1,7 @@
-const random_words = "rough present dislike place obtainable nippy mask reward doctor quilt quarter expect".split(
-  " "
-);
+const random_words = `rough present dislike place obtainable nippy mask reward doctor quilt quarter expect skeleton nut 
+haircut hour choice mechanism roof idea democratic goalkeeper resource grudge survey credit appeal helmet clash trance girl request restless 
+railroad other weight rotten roof coast firm illusion refrigerator`.split(" ");
 
-export default random_words;
+const cleansed_words = [...new Set(random_words.map(el => el.trim()))];
+
+export default cleansed_words;
