49bced6c0bbdf08e2322e401e0ff30543e66bca1
pandulaDW <pandula.official@gmail.com>
2020-02-02 18:42:24 +0530

    added more words

--- src/words.js
+++ src/words.js
@@ -1,6 +1,8 @@
 const random_words = `rough present dislike place obtainable nippy mask reward doctor quilt quarter expect skeleton nut 
 haircut hour choice mechanism roof idea democratic goalkeeper resource grudge survey credit appeal helmet clash trance girl request restless 
-railroad other weight rotten roof coast firm illusion refrigerator`.split(" ");
+railroad other weight rotten roof coast firm illusion refrigerator carrot`.split(
+  " "
+);
 
 const cleansed_words = [...new Set(random_words.map(el => el.trim()))];
 
